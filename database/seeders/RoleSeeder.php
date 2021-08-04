3525ac4d84a498cbc29080cf07dd374fa9575b35
Your Name <you@example.com>
2021-08-05 01:38:42 +0430

    complete roles seeder for memeber teacher and admin

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'title' => 'admin',
+        ]);
+
+        Role::create([
+            'title' => 'member',
+        ]);
+
+        Role::create([
+            'title' => 'teacher',
+        ]);
+    }
+}
