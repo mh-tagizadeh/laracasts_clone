6ebccfeaba2837bc8eb63237c92e80307e29effa
Your Name <you@example.com>
2021-08-11 04:17:43 +0430

    chanage database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
             RoleSeeder::class,
             PermissionSeeder::class,
         ]);
+        $category1 = Category::factory()->create();
+
+        $categoryX = Category::factory()->create();
+        
+        $categoryY = Category::factory()->for($categoryX)->create();
+
+        $category2 = Category::factory()->for($category1)->create();
+
+
+        $categories = Category::factory()
+            ->count(3)
+            ->for($category2)
+            ->create();  
     }
 }
