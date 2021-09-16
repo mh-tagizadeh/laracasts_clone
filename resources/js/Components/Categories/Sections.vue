8df636453cb92cf0d991c9dfb68617da33083c2f
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-09-16 16:13:14 +0430

    separate categories child and  parent in table

---
+++ resources/js/Components/Categories/Sections.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="flex flex-col my-10 mx-7 rounded-lg bg-indigo-600">
+    <Link :href="route('categories.parents')" class="ring-1 ring-white text-white py-2 text-center">
+        Parent Categories
+    </Link>
+    <Link :href="route('categories.index')" class="ring-1 ring-white text-white py-2 text-center">
+        Categories
+    </Link>
+    <Link :href="route('categories.create')" class="ring-1 ring-white text-white py-2 text-center">
+        Create Category
+    </Link>
+</div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default {
+    components: {
+        Link
+    }
+}
+</script>
--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -1,22 +1,20 @@
 <template>
     <app-layout title="Dashboard">
         <template #header>
-            <div class="w-full flex flex-row justify-between">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    Categories
-                </h2>
-                <div>
-                    <Link href="categories/create" class="shadow rounded-lg bg-blue-600 text-indigo-50 p-2">Create</Link>
-                </div>
-            </div>
+
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Categories
+            </h2>
+
         </template>
 
 
-        <div class="py-10">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <categories-table :categories="categories"/>
-                </div>
+        <div class="grid grid-cols-3">
+            <div>
+                <categories-sections></categories-sections>
+            </div>
+            <div class="col-span-2 my-10 pr-3">
+                <categories-table :categories="categories"/>
             </div>
         </div>
 
@@ -26,14 +24,16 @@
 <script>
     import AppLayout from '@/Layouts/AppLayout.vue'
     import { Head, Link } from '@inertiajs/inertia-vue3'
-    import CategoriesTable from '@/Jetstream/CategoriesTable.vue'
+    import CategoriesTable from '@/Components/Categories/Table.vue'
+    import CategoriesSections from '@/Components/Categories/Sections.vue'
 
     export default {
         components: {
             AppLayout,
             Head,
             Link,
-            CategoriesTable
+            CategoriesTable,
+            CategoriesSections
         },
         props: {
             categories: Object,
