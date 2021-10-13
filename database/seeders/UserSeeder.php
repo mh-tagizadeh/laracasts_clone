81cb6267fb84978507c046683f485669701d8039
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-13 11:19:27 +0330

    complete users seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,13 +15,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ]);
+        if (!User::where('email', 'admin@admin.com')->first())
+        {
+            $user = User::create([
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+        }
+        User::factory()->count(500)->create();
+        
+        User::chunk(200, function($users){
+            foreach ($users as $user) {
+                $user->assignRole('member');
+            }
+        });
+
 
     }
 }
