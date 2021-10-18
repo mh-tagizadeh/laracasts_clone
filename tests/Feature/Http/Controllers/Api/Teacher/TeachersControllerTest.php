48d47773636bfbbc9f13154051e0e513418a232a
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-18 16:17:18 +0330

    test can be update teacher with api

--- tests/Feature/Http/Controllers/Api/Teacher/TeachersControllerTest.php
+++ tests/Feature/Http/Controllers/Api/Teacher/TeachersControllerTest.php
@@ -35,4 +35,34 @@ class TeachersControllerTest extends TestCase
         // return data teacher 
         $response->dump();
     }
+
+    public function test_can_be_update_teacher_with_api()
+    {
+        $this->withoutExceptionHandling();
+
+        // create user and assign role teacher for him 
+        $user = User::factory()->create();
+
+        $teacher = Teacher::factory()->state(['username' => $user->name])->for($user)->create();
+
+        $user->assignRole('teacher');
+
+        // authentication teacher  
+        $this->actingAs($user);
+
+
+        $response = $this->json('POST', '/api/teachers/update', [
+            'first_name' => 'محمد',
+            'last_name' => 'اصلی',
+            'username' => $user->name,
+            'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, consequatur asperiores illum dolorum adipisci saepe optio numquam nam incidunt quibusdam, eaque, quas quo id consequuntur aliquid. Consequuntur consequatur temporibus tempora?',
+            'address' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste nobis commodi ullam sapiente expedita quas aliquam! Praesentium corporis libero ullam nostrum nihil cupiditate, nesciunt, laboriosam laudantium aut earum excepturi sed!',
+            'phone_number' => $teacher->phone_number,
+        ]);
+
+        $response->assertStatus(200);
+
+        //return data teacher
+        $response->dump();
+    }
 }
