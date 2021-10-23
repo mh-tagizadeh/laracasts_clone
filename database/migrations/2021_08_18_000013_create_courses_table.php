fce0d6d75c48c38b94ab3c80c5c0de47d59da094
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-23 22:46:36 +0330

    install telescope package

--- database/migrations/2021_08_18_000013_create_courses_table.php
+++ database/migrations/2021_08_18_000013_create_courses_table.php
@@ -17,11 +17,13 @@ class CreateCoursesTable extends Migration
             $table->id();
             $table->string('sku');
             $table->string('title');
+            // TODO: slug table must be uniqe
             $table->string('slug');
             $table->text('description');
             $table->integer('teacher_id');
             $table->integer('category_id');
             $table->boolean('state')->default(0);
+            // TODO: price table must be defual equal 0 
             $table->integer('price');
             $table->integer('sale_price');
             $table->timestamp('published_at')->nullable();
