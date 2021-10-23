4c1a58ff3d3cdae8b844cf58e2e799762f471d73
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-23 23:14:48 +0330

    add plan model.

---
+++ database/migrations/2021_10_23_191700_create_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('sale_price');
+            $table->integer('subscription_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
