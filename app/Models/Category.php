a3fa491e55ff483791d65ddbf19a11ca71500546
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-10-31 20:32:16 +0330

    add category model, counts lessons and courses

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -23,4 +23,39 @@ class Category extends Model
     {
         return $this->hasMany(Category::class);
     }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+
+    public function courses_count()
+    {
+        $count = 0; 
+
+        foreach ($this->categories_child as $cat)
+        {
+            $count += $cat->courses->count();
+        }
+
+        return $count;
+    }
+
+    public function lessons_count()
+    {
+        $count = 0; 
+
+        foreach ($this->categories_child as $cat)
+        {
+            foreach($cat->courses as $course)
+            {
+                $count += $course->lessons->count();
+            }
+        }
+
+        return $count;
+    }
+
+
 }
