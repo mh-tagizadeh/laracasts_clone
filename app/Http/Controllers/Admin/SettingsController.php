7ce515c98731a29e8ff164ce0d243be1674986eb
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-11-11 22:26:08 +0330

    create setting model and controller

---
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class SettingsController extends Controller
+{
+}
