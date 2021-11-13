aa42c522ff5c8f5b550575c028aaa01ba9153a40
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-11-13 16:16:46 +0330

    create plans section in dashboard

---
+++ resources/js/Components/Plans/Sections.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="flex flex-col my-10 mx-7 rounded-lg bg-indigo-600">
+    <Link :href="route('plans.index')" class="ring-1 ring-white text-white py-2 text-center">
+        Plans
+    </Link>
+    <Link :href="route('plans.create')" class="ring-1 ring-white text-white py-2 text-center">
+        Create Plan
+    </Link>
+</div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default {
+    components: {
+        Link
+    }
+}
+</script>
---
+++ resources/js/Pages/Plans/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+		<div class="grid grid-cols-3">
+            <div>
+            	<plan-sections></plan-sections>
+            </div>
+            <div class="col-span-2 my-10 pr-3">
+            </div>
+        </div>
+
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import PlanSections from '@/Components/Plans/Sections.vue'
+
+    export default {
+        components: {
+            AppLayout,
+            PlanSections,
+        },
+    }
+</script>
+
+
+	
