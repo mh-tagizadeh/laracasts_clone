3a8252782a4d06d85bc5eb14c61d17fb09046b98
Mohammadhosein Tagizadeh <mohammadhusseinbvb09@gmail.com>
2021-11-16 22:36:59 +0330

    clean code with model layer

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -14,4 +14,21 @@ class Course extends Model
     protected $fillable = [
         'sku', 'title', 'slug', 'description', 'teacher_id','state', 'published_at', 'category_id'
     ];
+
+    public static function get_courses_paginate()
+    {
+        $courses = Course::paginate(20)->through(function ($course) {
+            return [
+                'id' => $course->id,
+                'sku' => $course->sku,
+                'title' => $course->title,
+                'category' => $course->category->name,
+                'teacher' => $course->teacher->username,
+                'status' => $course->status,
+                'image' => $course->image ? $course->image->url : '/img/logo.svg',
+            ];
+        });
+
+        return $courses;
+    }
 }
