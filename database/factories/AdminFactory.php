dac5d708768c1093ee2235a6441ffce5ccc3c8cf
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-08-12 16:32:23 +0430

    complete admin factory

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class AdminFactory extends Factory
 {
@@ -14,7 +15,11 @@ class AdminFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->username(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
