93f8375832a750eddc283d105591ec55aab9be95
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-08-12 16:31:39 +0430

    add min validation for name field login

--- app/Http/Requests/Admin/LoginAdminRequest.php
+++ app/Http/Requests/Admin/LoginAdminRequest.php
@@ -24,7 +24,7 @@ class LoginAdminRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required|min:2',
             'password' => 'required',
         ];
     }
