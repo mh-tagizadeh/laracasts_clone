47e984b698d425eb7bf48a702d60b3149f3ad216
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-08-12 16:31:50 +0430

    fixed error for route prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('management')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
+
         });
     }
 
